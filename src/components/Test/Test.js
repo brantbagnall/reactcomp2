1cc81f7871e9757e92ecc9a8705746b8b7f5cbe7
Brant Bagnall <brant.bagnall@gmail.com>
2017-12-18 11:00:28 -0700

    mentor check-in 11:00

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -4,6 +4,7 @@ export default class Test extends Component {
   render() {
     return (
       <div>
+          {/* COMP 42J and COMP 42K */}
         <h1>Your id is {this.props.match.params.id}</h1>
       </div>
     )
